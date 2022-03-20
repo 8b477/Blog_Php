d6626cb2885104e02de59f38467cc50cda1c9351
8b477 <jonathan_buchet@outlook.fr>
2022-03-20 15:44:26 +0100

    1.4 create space for user

--- public/inscription.php
+++ public/inscription.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\config\Connect;
 use App\config\FunctionManager;
 
 require_once (__DIR__ . '/../include.php');
@@ -12,20 +13,59 @@ require_once (__DIR__ . '/../include.php');
         if (empty($_POST['username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['username']))
         {
             $errors['username'] = "Votre pseudo n'est pas valide caractères de (a) à (z) de 0 à 9 et underscore autorisé";
+        }else{
+            $pdo = Connect::getPDO()->prepare('SELECT id FROM users WHERE username = ?');
+            $pdo->execute([$_POST['username']]);
+            $user = $pdo->fetch();
+
+            if ($user)
+            {
+                $errors['username'] = "ce speudo est déjà utilisé !";
+            }
         }
         if (empty($_POST['mail']) || !filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL))
         {
             $errors['mail'] = "Votre adresse mail n'est pas valide !";
+        }else{
+            $pdo = Connect::getPDO()->prepare('SELECT id FROM users WHERE mail = ?');
+            $pdo->execute([$_POST['mail']]);
+            $mail = $pdo->fetch();
+            if ($mail)
+            {
+                $errors['mail'] = "L'adresse mail est déjà utilisé !";
+            }
         }
-        if (empty($_POST['pass']) || $_POST['pass'] != $_POST['pass-check'])
+        if (empty($_POST['password']) || $_POST['password'] != $_POST['password-check'])
         {
-            $errors['pass'] = "Veuillez rentrer un mot de passe valide !";
+            $errors['password'] = "Veuillez rentrer un mot de passe valide !";
         }
-        FunctionManager::debug($errors);
+
+        if (empty($errors))
+        {
+            $pdo = Connect::getPDO();
+            $req = $pdo->prepare("INSERT INTO users SET username = ?, password = ?, mail = ?");
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $req->execute([$_POST['username'], $password, $_POST['mail']]);
+            die("Votre compte à bien été créé !");
+        }
+
     }
 
 ?>
-
+<?php
+    if (!empty($errors))
+    { ?>
+        <div class="alert alert-danger">
+            <p>Vous n'avez pas rempli le formulaire correctement</p>
+            <ul>
+                <?php foreach ($errors as $error)
+                {?>
+                    <li><?= $error; ?></li>
+                <?php } ?>
+            </ul>
+        </div>
+    <?php }
+?>
 <form action="" method="post">
 
     <label for="username-id">Entrez un pseudo : </label>
@@ -34,11 +74,11 @@ require_once (__DIR__ . '/../include.php');
     <label for="mail-id">Entrez votre adresse mail : </label>
     <p><input type="mail" name="mail" id="mail-id"></p>
 
-    <label for="pass-id">Entrez un mot de passe : </label>
-    <p><input type="password" name="pass" id="pass-id"></p>
+    <label for="password-id">Entrez un mot de passe : </label>
+    <p><input type="password" name="password" id="password-id"></p>
 
     <label for="pass-id-check">Confirmez le mot de passe : </label>
-    <p><input type="password" name="pass-check" id="pass-id-check"></p>
+    <p><input type="password" name="password-check" id="password-id-check"></p>
 
     <p><input type="submit" value="inscription"></p>
 
